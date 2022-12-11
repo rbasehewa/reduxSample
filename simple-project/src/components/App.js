8ed2d89459f06d57979bad36a9edcc085e6f4ee6
rbasehewa <rbasehewa@gmail.com>
2022-12-11 20:18:55 +1100

    before adding store

---
+++ simple-project/src/components/App.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import ButtonComponent from './ButtonComponent'; 
+
+function App() {
+  return (
+    <div className="App">
+
+      <h2>hello</h2>
+      <ButtonComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ simple-project/src/components/ButtonComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class buttonComponents extends Component {
+  render() {
+    return (
+      <div>
+        <button>Click Me</button>
+      </div>
+    )
+  }
+}
